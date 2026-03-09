c8c160d21888dfd93c7f7029addeaa7864ae3aca
Catalogoar <catalogosimple01@gmail.com>
2026-03-09 10:14:52 -0300

    Update products.js

--- products.js
+++ products.js
@@ -12,63 +12,63 @@ const PRODUCTS = [
         name: "Almohadones Bordado Chino",
         description: "Diseños bordados artesanales",
         price: 15000,
-        image: "https://tu-dominio.com/imagenes/almohadones_chino.jpg"
+        image: "images/almohadones_chino.jpg"
     },
     {
         id: 3,
         name: "Cortinas Capitone",
         description: "Colores y medidas a elección",
         price: 30000,
-        image: "https://tu-dominio.com/imagenes/cortinas.jpg"
+        image: "images/cortinas.jpg"
     },
     {
         id: 4,
         name: "Toallas de baño en cinta",
         description: "Variedades de color",
         price: 15000,
-        image: "https://tu-dominio.com/imagenes/toallas.jpg"
+        image: "images/toallas.jpg"
     },
     {
         id: 5,
         name: "Almohadones Capitone",
         description: "Diseños artesanales",
         price: 15000,
-        image: "https://tu-dominio.com/imagenes/almohadones_capi.jpg"
+        image: "images/almohadones_capi.jpg"
     },
     {
         id: 6,
         name: "Toallitas de mano bordadas",
         description: "Colores surtidos",
         price: 7000,
-        image: "https://tu-dominio.com/imagenes/toallitas.jpg"
+        image: "images/toallitas.jpg"
     },
     {
         id: 7,
         name: "Almohadones cubre sillas",
         description: "Medidas y colores a elección",
         price: 13000,
-        image: "https://tu-dominio.com/imagenes/cubresillas.jpg"
+        image: "images/cubresillas.jpg"
     },
     {
         id: 8,
         name: "Juego de 2 almohadones capitone",
         description: "40x40 cm con diseños resaltados",
         price: 25000,
-        image: "https://tu-dominio.com/imagenes/almohadones_dise.jpg"
+        image: "images/almohadones_dise.jpg"
     },
     {
         id: 9,
         name: "Almohadas para cama",
         description: "60x40 cm",
         price: 40000,
-        image: "https://tu-dominio.com/imagenes/almohadas.jpg"
+        image: "images/almohadas.jpg"
     },
     {
         id: 10,
         name: "Bolsos de dama",
         description: "Diseños y colores a elección",
         price: 20000,
-        image: "https://tu-dominio.com/imagenes/bolsos.jpg"
+        image: "images/bolsos.jpg"
     }
 ];
 
